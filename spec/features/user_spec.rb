cef4aeb47bb2eeddaa3f22e7780d3d85d6855b3a
Oleg Voloshyn <voloshyn@active-bridge.com>
2016-07-27 17:19:58 +0300

    Add capybara tests for Sign Up/ Sign Out with vk and fb

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -4,10 +4,21 @@ feature 'User' do
   let(:user) { create(:user) }
   let(:welcome_page) { WelcomePage.new }
 
-  scenario 'Sign Up / user edit', js: true do
+  scenario 'Sign Up / Sign Out user with facebook', js: true do
     welcome_page.open
     expect(page).to have_content('Зареєструватися')
-    welcome_page.login_with(:facebook)
+    welcome_page.login_with_facebook
     expect(page).to have_content('Мій профіль')
+    click_link('Вийти')
+    expect(page).to have_content('Зареєструватися')
+  end
+
+  scenario 'Sign Up / Sign Out user with vk', js: true do
+    welcome_page.open
+    expect(page).to have_content('Зареєструватися')
+    welcome_page.login_with_vk
+    expect(page).to have_content('Мій профіль')
+    click_link('Вийти')
+    expect(page).to have_content('Зареєструватися')
   end
 end
--- spec/support/pages/welcome_page.rb
+++ spec/support/pages/welcome_page.rb
@@ -1,15 +1,27 @@
 require 'support/base_page'
 
 class WelcomePage < BasePage
-  def login_with(provider, options = { success: true })
+  def login_with_facebook(options = { success: true })
     click_link 'Зареєструватися'
     if options[:success]
-      OmniAuth.config.mock_auth[provider] = OmniAuth::AuthHash.new(provider: provider, uid: '12345', nname: 'test_username',
-                                                                   info: { name: 'test_username', image: 'http://link_to_image.com/image' },
-                                                                   credentials: { token: :token, secret: :secret })
+      OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new(provider: 'facebook', uid: '12345', nname: 'test_username',
+                                                                    info: { name: 'test_username', image: 'http://link_to_image.com/image' },
+                                                                    credentials: { token: :token, secret: :secret })
     else
-      OmniAuth.config.mock_auth[provider] = :invalid_credentials
+      OmniAuth.config.mock_auth[:facebook] = :invalid_credentials
     end
     click_link 'Увійти через Facebook'
+  end
+
+  def login_with_vk(options = { success: true })
+    click_link 'Зареєструватися'
+    if options[:success]
+      OmniAuth.config.mock_auth[:vkontakte] = OmniAuth::AuthHash.new(provider: 'vkontakte', uid: '12345', nname: 'test_username',
+                                                                    info: { name: 'test_username', image: 'http://link_to_image.com/image' },
+                                                                    credentials: { token: 'sf23hdsjk', secret: :secret }, extra: { raw_info: { photo_200_orig: 'http://link_to_image.com/image' }})
+    else
+      OmniAuth.config.mock_auth[:vkontakte] = :invalid_credentials
+    end
+    click_link 'Увійти через VK'
   end
 end
