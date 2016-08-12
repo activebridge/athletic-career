7327a2c18f76db717020d592126f5a273dfe8c13
TupickiyV <vladislav.tupickiy@gmail.com>
2016-08-12 12:02:07 +0300

    Fix header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def current_locale?(locale)
     I18n.locale == locale
   end
+
+  def nav_link(text, path)
+    link_to_unless_current text, path do
+      content_tag :span, text
+    end
+  end
 end
