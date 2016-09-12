b921495a44f8e66f2104dbcaa765696bcfc65664
TupickiyV <vladislav.tupickiy@gmail.com>
2016-09-12 17:52:15 +0300

    Add style to User#show & User#index pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,10 @@ class UsersController < ApplicationController
   skip_before_action :require_user, only: [:new, :create]
   expose :users, -> { User.all }
   expose :user
+
+  def index; end
+
+  def show
+    @competitions = Competition.user_competition(user.id)
+  end
 end
--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,4 +1,6 @@
 class Competition < ApplicationRecord
   belongs_to :user
   belongs_to :event
+
+  scope :user_competition, -> (user_id) { where('user_id = ?', user_id) }
 end
