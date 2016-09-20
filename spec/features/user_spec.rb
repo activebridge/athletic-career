5945b23f452638896adcdde06367246e049e2385
Oleg Voloshyn <voloshyn@active-bridge.com>
2016-09-20 20:47:56 +0300

    remove specs

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -3,26 +3,4 @@ require 'rails_helper'
 feature 'User' do
   let(:user) { create(:user) }
   let(:welcome_page) { WelcomePage.new }
-
-  # scenario 'Sign Up / Sign Out user with facebook', js: true do
-  #   welcome_page.open
-  #   expect(page).to have_content('Увійти')
-  #   welcome_page.login_with_facebook
-  #   expect(page).to have_content('test_username')
-  #   click_link('test_username')
-  #   expect(page).to have_content('Вийти')
-  #   click_link('Вийти')
-  #   expect(page).to have_content('Увійти')
-  # end
-
-  # scenario 'Sign Up / Sign Out user with vk', js: true do
-  #   welcome_page.open
-  #   expect(page).to have_content('Увійти')
-  #   welcome_page.login_with_vk
-  #   expect(page).to have_content('test_username')
-  #   click_link('test_username')
-  #   expect(page).to have_content('Вийти')
-  #   click_link('Вийти')
-  #   expect(page).to have_content('Увійти')
-  # end
 end
