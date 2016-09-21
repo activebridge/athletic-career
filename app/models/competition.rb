ab767996d765ce2358ef60fbbc870b499062bebf
TupickiyV <vladislav.tupickiy@gmail.com>
2016-09-21 12:29:24 +0300

    Add validates

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -2,4 +2,7 @@ class Competition < ApplicationRecord
   belongs_to :user
   belongs_to :event
   belongs_to :distance
+
+  validates :year, :city, :event_id, :distance_id, :race_number, :result, :net_result, :rank, :category_rank, presence: true
+  validates :category_rank, numericality: { less_than_or_equal_to: :rank }
 end
