95f4f4c30a46eceb40b8d802c8d1c2ff46bbcad8
TupickiyV <vladislav.tupickiy@gmail.com>
2016-09-22 11:07:34 +0300

    Add mark to Length

--- app/models/length.rb
+++ app/models/length.rb
@@ -1,7 +1,7 @@
 class Length < ApplicationRecord
   validates :title, presence: true, uniqueness: true
 
-  scope :ready, -> { where(visible: true) }
+  scope :ready, -> { where(visible: true).order(mark: :asc) }
 
   has_many :distances
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,41 @@ unless Rails.env.production?
     )
   end
 
-  (1..5).each do |i|
-    puts 'Lengths was successfully created'
-    Length.create(
-      title: "Дистанция_#{i}",
-      visible: true
-    )
-  end
+  Length.create(
+    title: 'ultra',
+    visible: true,
+    mark: 1
+  )
+  Length.create(
+    title: '42.2',
+    visible: true,
+    mark: 2
+  )
+  Length.create(
+    title: '21.1',
+    visible: true,
+    mark: 3
+  )
+  Length.create(
+    title: '10',
+    visible: true,
+    mark: 4
+  )
+  Length.create(
+    title: '5',
+    visible: true,
+    mark: 5
+  )
+  Length.create(
+    title: '3',
+    visible: true,
+    mark: 6
+  )
+  Length.create(
+    title: '1',
+    visible: true,
+    mark: 7
+  )
 
   (1..5).each do |i|
     puts 'Organizers was successfully created'
