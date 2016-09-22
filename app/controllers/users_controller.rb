7abe0b896d300927e786f17cdc1ca00a08343b36
Oleg Voloshyn <voloshyn@active-bridge.com>
2016-09-22 22:28:47 +0300

    update competitions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,7 @@
 class UsersController < ApplicationController
   skip_before_action :require_user, only: [:new, :create]
-  before_action :competitions, only: :show
 
   expose :users, -> { User.all }
   expose :user
-
-  def index; end
-
-  private
-
-  def competitions
-    @competitions ||= user.competitions
-  end
+  expose :competitions, -> { user.competitions }
 end
