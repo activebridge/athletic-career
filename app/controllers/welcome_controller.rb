822f43c9b36bdfe72157d05a54712905eee4ff32
TupickiyV <vladislav.tupickiy@gmail.com>
2016-09-28 22:43:24 +0300

    Fix past and future event

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,5 @@ class WelcomeController < ApplicationController
   layout 'before_start'
   skip_before_action :require_user
 
-  def index
-    @events = Event.ready
-  end
+  expose :events, -> { Event.ready }
 end
