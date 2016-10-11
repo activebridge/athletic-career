b33b33eb4f178764f646719db9ee2b8fac745632
TupickiyV <vladislav.tupickiy@gmail.com>
2016-10-11 14:25:32 +0300

    Add social share buttons

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
-  skip_before_action :require_user, only: [:new, :create]
+  skip_before_action :require_user, only: [:new, :create, :show]
 
   expose :users, -> { User.all }
   expose :user, -> { User.friendly.find(params[:id]) }
   expose :competitions, -> { user.competitions.includes(:event) }
+
+  def root_link
+    "#{request.protocol}#{request.host}"
+  end
+  helper_method :root_link
 end
