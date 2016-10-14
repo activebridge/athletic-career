294e1e4760b5db7d8632b76fc7a98adced735e15
Oleg Voloshyn <voloshyn@active-bridge.com>
2016-10-14 23:32:32 +0300

    fix deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :repo_url, 'git@github.com:activebridge/athletic-career.git'
 set :deploy_to, '/var/www/athletic-career'
 
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/cloudinary.yml')
-set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 set :rvm_type, :user
 set :rvm_ruby_version, '2.3.1'
