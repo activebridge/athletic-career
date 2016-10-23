1be152fd7206f586f3b0280b7ddb096c9964becd
Oleg Voloshyn <voloshyn@active-bridge.com>
2016-10-23 21:28:24 +0300

    update navbar js

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,9 +1,9 @@
 $(window).scroll(function() {
-    if ($('.navbar').offset().top > 100) {
-        $('.navbar-fixed-top').addClass('top-nav');
-        $('.pull-left').addClass('logo');
-    } else {
-        $('.navbar-fixed-top').removeClass('top-nav');
-        $('.pull-left').removeClass('logo');
-    }
+  if ($('.navbar').offset().top > 100) {
+    $('.navbar-fixed-top').addClass('top-nav');
+    $('.pull-left').addClass('logo');
+  } else {
+    $('.navbar-fixed-top').removeClass('top-nav');
+    $('.pull-left').removeClass('logo');
+  }
 });
