888a0db4ea0f8bb4751ca8c398e9fbcf76a10cca
Oleg Voloshyn <voloshyn@active-bridge.com>
2016-12-06 13:50:50 +0200

    add ability update user profile on admin dashboard

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,8 +2,19 @@ class Admin::UsersController < AdminsController
   expose :users, -> { User.all }
   expose :user, -> { User.friendly.find(params[:id]) }
 
+  def update
+    return redirect_to admin_users_path if user.update(user_params)
+    render :edit
+  end
+
   def destroy
     user.destroy
     redirect admin_dashboard_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit!
   end
 end
