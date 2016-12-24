892f44e6ea374756d9da5fb9ba8352889826992b
Oleg Voloshyn <voloshyn@active-bridge.com>
2016-12-24 20:12:51 +0200

    add columns to Organizer's members table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161215191012) do
+ActiveRecord::Schema.define(version: 20161224175451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -139,6 +139,8 @@ ActiveRecord::Schema.define(version: 20161215191012) do
     t.string   "bio"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "email"
+    t.string   "phone"
     t.index ["organizer_id"], name: "index_members_on_organizer_id", using: :btree
   end
 
