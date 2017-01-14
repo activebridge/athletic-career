9b7c1a5347b18f02742e80d2d20c5e310959979d
Oleg Voloshyn <voloshyn@active-bridge.com>
2017-01-14 20:09:50 +0200

    add results button for admin

--- app/controllers/admin/results_controller.rb
+++ app/controllers/admin/results_controller.rb
@@ -1,15 +1,16 @@
 class Admin::ResultsController < AdminsController
   def import_csv
-    parse_csv(params['csv']['file'].tempfile)
+    parse_csv(params['csv']['file'].tempfile, params[:distance].to_i)
     redirect_to admin_results_path
   end
 
   private
 
-  def parse_csv(file)
+  def parse_csv(file, distance)
+    binding.pry
     results = CSV.foreach(file, headers: false)
     results.each do |result|
-      r = Result.find_or_initialize_by(name: result[0], bib_number: result[1], age: result[2], category: result[3], gun_time: result[4], chip_time: result[5], position: result[6], category_position: result[7], gender: result[8])
+      r = Result.find_or_initialize_by(distance_id: distance, position: result[0], bib_number: result[1], name: result[2], category: result[3], category_position: result[4], gun_time: result[5], chip_time: result[6], gender: result[7], age: result[8])
       r.save(validate: false)
     end
   rescue
