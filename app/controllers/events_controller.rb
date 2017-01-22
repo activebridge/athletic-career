d233c8c1d159bee3902eff0470ef1f2c42ebc34e
Oleg Voloshyn <voloshyn@active-bridge.com>
2017-01-22 17:54:17 +0200

    fix order for organizer_events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,7 +9,7 @@ class EventsController < ApplicationController
   expose :index_events, -> { params[:type] == 'past' ? past_events : future_events }
   expose :competitions_list, -> { show_event.competitions.includes(:user) }
   expose :distances_list, -> { show_event.distances.includes(:length) }
-  expose :organizer_events, -> { show_event.organizer.events.ready.where('events.id != ?', show_event.id).last(4) }
+  expose :organizer_events, -> { show_event.organizer.events.ready.where('events.id != ?', show_event.id).last(4).order('date desc') }
 
   def new
     event.distances.build
