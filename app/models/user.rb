f607dca65c00e375bba7d5ff0014701815fa8ebd
Oleg Voloshyn <voloshyn@active-bridge.com>
2017-01-30 14:51:30 +0200

    improve user for sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,9 +16,14 @@ class User < ApplicationRecord
 
   def self.fetch_account(account_params, user_params)
     user = find_by_provider_uid(account_params[:provider], account_params[:uid])
-    return user.first if user.present?
+    return improve(user.first, user_params) if user.present?
     return if find_by('lower(name) = ?', account_params[:name].downcase)
     user = create(user_params)
+    user
+  end
+
+  def self.improve(user, user_params)
+    user.update(token: user_params[:token], remote_avatar_url: user_params[:remote_avatar_url], name: user_params[:name])
     user
   end
 
