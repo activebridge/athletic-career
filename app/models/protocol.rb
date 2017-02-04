914125ed3b3d74619f8d06c8f0e12267233217f9
Oleg Voloshyn <voloshyn@active-bridge.com>
2017-02-04 16:05:21 +0200

    add Protocol functionality

--- app/models/protocol.rb
+++ app/models/protocol.rb
@@ -1,3 +1,10 @@
 class Protocol < ApplicationRecord
   belongs_to :user
+  validates :year, presence: true
+  validates :first_name_ua, presence: true, if: :last_name_ua?
+  validates :last_name_ua, presence: true, if: :first_name_ua?
+  validates :first_name_ru, presence: true, if: :last_name_ru?
+  validates :last_name_ru, presence: true, if: :first_name_ru?
+  validates :first_name_en, presence: true, if: :last_name_en?
+  validates :last_name_en, presence: true, if: :first_name_en?
 end
