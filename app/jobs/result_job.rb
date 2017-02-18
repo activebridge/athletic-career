f1be9ecd698fef32aa84226bd774d0ff5b6122ac
Oleg Voloshyn <voloshyn@active-bridge.com>
2017-02-18 19:50:04 +0200

    add Result Job for calculate next protocols

--- app/jobs/result_job.rb
+++ app/jobs/result_job.rb
@@ -1,20 +1,20 @@
-class LoginJob < ActiveJob::Base
+class ResultJob < ActiveJob::Base
   include ResultMethods
 
-  def perform(protocol)
+  def perform(protocol, results)
     # year = protocol.birthday.year
     list = list(protocol)
     return if list.blank?
-    competitions = competitions(list)
+    competitions = competitions(list, results)
     return if competitions.blank?
     finish(competitions, protocol)
   end
 
   private
 
-  def competitions(list)
+  def competitions(list, results)
     competitions = []
-    list.each { |name| competitions += Result.where(name: name) }
+    list.each { |name| competitions += results.where(name: name) }
     competitions
   end
 end
